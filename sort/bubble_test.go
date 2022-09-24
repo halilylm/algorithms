54479e24e2e20e7426a1d5011addb04e6fe3e31a
halilylm <65048618+halilylm@users.noreply.github.com>
2022-09-24 18:52:49 +0000

    bubble sort implented

---
+++ sort/bubble_test.go
@@ -0,0 +1,15 @@
+package sort
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestBubbleSort(t *testing.T) {
+	arr := []int{4, -2, 6, 5}
+	BubbleSort(&arr)
+	expected := []int{-2, 4, 5, 6}
+	if !reflect.DeepEqual(arr, expected) {
+		t.Errorf("expected %v got %v", expected, arr)
+	}
+}
